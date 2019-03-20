4934e63dc345f738ab1ce5f446e4e98a151e15d7
Kiselev Denis <denis.kiselev.1997@gmail.com>
2019-03-21 01:14:07 +0300

    Task2 done

--- modules/task_2/kiselev_d_monte_carlo_task2_omp/main.cpp
+++ modules/task_2/kiselev_d_monte_carlo_task2_omp/main.cpp
@@ -29,68 +29,64 @@ double ellipsoid(double x, double y, double z) {
 }
 
 int main(int argc, char *argv[]) {
-    int nPointsInEllipsoid;
-    double t1, avgValueOfHits, result;
-    int nPoints = (argc == 1) ? DEFAULT_NPOINTS : atoi(argv[1]);
-
     // Creating a generator and distributions
     // for random double numbers in the ranges
     std::default_random_engine generator;
     std::uniform_real_distribution<> xAxisDistr(X1, X2);
     std::uniform_real_distribution<> yAxisDistr(Y1, Y2);
     std::uniform_real_distribution<> zAxisDistr(Z1, Z2);
+    int nPoints = (argc == 1) ? DEFAULT_NPOINTS : atoi(argv[1]);
 
 
 
     // Sequential code
-    t1 = omp_get_wtime();
-    nPointsInEllipsoid = 0;
+    double t1 = omp_get_wtime();
+    int nPointsInEllipsoid = 0;
     generator.seed(0);
     for (int i = 0; i < nPoints; i++) {
         double value = ellipsoid(xAxisDistr(generator),
                                  yAxisDistr(generator),
                                  zAxisDistr(generator));
-        if (value <= 0) {
-            nPointsInEllipsoid++;
-        }
+        if (value <= 0) nPointsInEllipsoid++;
     }
-    avgValueOfHits = static_cast<double>(nPointsInEllipsoid) / nPoints;
-    result = (X2 - X1) * (Y2 - Y1) * (Z2 - Z1) * avgValueOfHits;
+    double avgValueOfHits = static_cast<double>(nPointsInEllipsoid) / nPoints;
+    double result = (X2 - X1) * (Y2 - Y1) * (Z2 - Z1) * avgValueOfHits;
     double seqTime = omp_get_wtime() - t1;
     std::cout.precision(8);
+    std::cout << std::fixed;
     std::cout << "Sequential alg:" << std::endl
-              << "\tResult: " << std::fixed << result << std::endl
-              << "\tTime: " << std::fixed << seqTime << std::endl << std::endl;
+              << "\tResult: " << result << std::endl
+              << "\tTime: " << seqTime << " sec" << std::endl << std::endl;
 
 
 
     // Parallel code
     t1 = omp_get_wtime();
     nPointsInEllipsoid = 0;
-    //#pragma omp parallel firstprivate(generator) reduction(+: nPointsInEllipsoid)
-    //{
+    #pragma omp parallel private(generator) reduction(+: nPointsInEllipsoid)
+    {
         generator.seed(omp_get_thread_num());
-        #pragma omp parallel for schedule(static) reduction(+: nPointsInEllipsoid) firstprivate(generator)
+        #pragma omp for schedule(static)
         for (int i = 0; i < nPoints; i++) {
             double value = ellipsoid(xAxisDistr(generator),
                                      yAxisDistr(generator),
                                      zAxisDistr(generator));
-            if (value <= 0) {
-                nPointsInEllipsoid++;
-            }
+            if (value <= 0) nPointsInEllipsoid++;
         }
-    //}
+    }
     avgValueOfHits = static_cast<double>(nPointsInEllipsoid) / nPoints;
     result = (X2 - X1) * (Y2 - Y1) * (Z2 - Z1) * avgValueOfHits;
     double parTime = omp_get_wtime() - t1;
     std::cout << "Parallel alg:" << std::endl
-              << "\tResult: " << std::fixed << result << std::endl
-              << "\tTime: " << std::fixed << parTime << std::endl << std::endl;
+              << "\tResult: " << result << std::endl
+              << "\tTime: " << parTime << " sec" << std::endl << std::endl;
+
 
 
     double realRes = 4.0 / 3.0 * std::acos(-1) * ELLPS_A * ELLPS_B * ELLPS_C;
     double acceleration = seqTime / parTime;
-    std::cout << "Acceleration: " << std::fixed << acceleration << std::endl
-              << "Real result: " << std::fixed << realRes << std::endl;
+    std::cout << "Real result: " << realRes << std::endl
+              << "Acceleration: " << acceleration << std::endl;
+              
     return 0;
 }
