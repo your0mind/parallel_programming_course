34681f8be6aa837936afe6871a190dce028eee03
Kiselev Denis <37990101+your0mind@users.noreply.github.com>
2019-03-22 12:35:44 +0300

    Киселев Денис. Задача 2. Вычисление многомерных интегралов методом Монте-Карло. (#94)
    * Added files
    
    * Task2 done
    
    * Вынес операции интегрирования в отдельную функцию.
    
    * Temp.
    
    * Добавлена отдельная функция для последовательного интегрирования.
    
    * Изменил сигнатуру интегрируемой функции.
    
    * Added <utility>, <vector>.

---
+++ modules/task_2/kiselev_d_monte_carlo_task2_omp/main.cpp
@@ -0,0 +1,117 @@
+// Copyright Kiselev Denis 2019
+
+#include <omp.h>
+
+#include <cfloat>
+#include <functional>
+#include <iostream>
+#include <random>
+#include <utility>
+#include <vector>
+
+#define DEFAULT_NPOINTS 100000
+
+// Ellipsoid options
+#define ELLPS_A 1.0
+#define ELLPS_B 2.0
+#define ELLPS_C 3.0
+
+// Limits of integration
+#define X1 -1.0
+#define X2 1.0
+#define Y1 -2.0
+#define Y2 2.0
+#define Z1 -3.0
+#define Z2 3.0
+
+double ellipsoid(const std::vector<double>& args) {
+    return -1.0 + pow(args[0] / ELLPS_A, 2)
+                + pow(args[1] / ELLPS_B, 2)
+                + pow(args[2] / ELLPS_C, 2);
+}
+
+double integrateByMonteCarlo(
+        std::function<double(const std::vector<double>&)> func,
+        std::vector<std::pair<double, double> > limits, int nPoints) {
+    int dimension = limits.size();
+    std::vector<std::uniform_real_distribution<> > distrs(dimension);
+
+    double measure = 1.0;
+    for (int i = 0; i < dimension; i++) {
+        measure *= limits[i].second - limits[i].first;
+        distrs[i].param(std::uniform_real_distribution<>::param_type(
+            limits[i].first, limits[i].second));
+    }
+
+    int nPointsInEllipsoid = 0;
+    std::vector<double> args(dimension);
+    std::default_random_engine generator(omp_get_thread_num());
+    for (int i = 0; i < nPoints; i++) {
+        for (int j = 0; j < dimension; j++)
+            args[j] = distrs[j](generator);
+        double value = func(args);
+        if (value <= 0) nPointsInEllipsoid++;
+    }
+
+    double avgValueOfHits = static_cast<double>(nPointsInEllipsoid) / nPoints;
+    return measure * avgValueOfHits;
+}
+
+double integrateByMonteCarloParallel(
+        std::function<double(std::vector<double>&)> func,
+        std::vector<std::pair<double, double> > limits, int nPoints) {
+    int dimension = limits.size();
+    std::vector<std::uniform_real_distribution<> > distrs(dimension);
+
+    double measure = 1.0;
+    #pragma omp parallel for reduction(*: measure)
+    for (int i = 0; i < dimension; i++) {
+        measure *= limits[i].second - limits[i].first;
+        distrs[i].param(std::uniform_real_distribution<>::param_type(
+            limits[i].first, limits[i].second));
+    }
+
+    int nPointsInEllipsoid = 0;
+    #pragma omp parallel reduction(+: nPointsInEllipsoid)
+    {
+        std::vector<double> args(dimension);
+        std::default_random_engine generator(omp_get_thread_num());
+        #pragma omp for schedule(static)
+        for (int i = 0; i < nPoints; i++) {
+            for (int j = 0; j < dimension; j++)
+                args[j] = distrs[j](generator);
+            double value = func(args);
+            if (value <= 0) nPointsInEllipsoid++;
+        }
+    }
+    double avgValueOfHits = static_cast<double>(nPointsInEllipsoid) / nPoints;
+    return measure * avgValueOfHits;
+}
+
+int main(int argc, char *argv[]) {
+    int nPoints = (argc == 1) ? DEFAULT_NPOINTS : atoi(argv[1]);
+
+    // Sequential
+    double t1 = omp_get_wtime();
+    double seqResult = integrateByMonteCarlo(
+        ellipsoid, { { X1, X2 }, { Y1, Y2 }, { Z1, Z2 } }, nPoints);
+    double seqTime = omp_get_wtime() - t1;
+
+    // Parallel
+    t1 = omp_get_wtime();
+    double parResult = integrateByMonteCarloParallel(
+        ellipsoid, { { X1, X2 }, { Y1, Y2 }, { Z1, Z2 } }, nPoints);
+    double parTime = omp_get_wtime() - t1;
+
+    double realRes = 4.0 / 3.0 * std::acos(-1) * ELLPS_A * ELLPS_B * ELLPS_C;
+    double acceleration = seqTime / parTime;
+    std::cout << "Sequential alg:" << std::endl
+              << "\tResult: " << seqResult << std::endl
+              << "\tTime: " << seqTime << " sec" << std::endl
+              << "Parallel alg:" << std::endl
+              << "\tResult: " << parResult << std::endl
+              << "\tTime: " << parTime << " sec" << std::endl
+              << "Real result: " << realRes << std::endl
+              << "Acceleration: " << acceleration << std::endl;
+    return 0;
+}
